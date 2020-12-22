2f21f3c54967c8e71be15b10c0295a8214131b88
FenUwU <76456800+fenuwu@users.noreply.github.com>
2020-12-22 18:17:41 -0500

    added error handling to config loader

--- helpers/bot_config.py
+++ helpers/bot_config.py
@@ -1,10 +1,27 @@
 import json
+import sys
+
 
 def load():
-    with open('config.json', 'r') as f:
-        data = json.load(f)
+    filename = 'config.json'
+    
+    try:
+        with open(filename, 'r') as f:
+            data = json.load(f)
         
-    return {
-        'prefix' : data['prefix'],
-        'token'  : data['token']
-    }
+        return {
+            'prefix' : data['prefix'],
+            'token'  : data['token']
+        }
+
+    except FileNotFoundError:
+        print(f'\'{filename}\' does not exist.')
+        sys.exit()
+
+    except json.decoder.JSONDecodeError:
+        print(f'\'{filename}\' is malformed.')
+        sys.exit()
+
+    except KeyError:
+        print(f'\'{filename}\' is missing information.')
+        sys.exit()
