4ae2cf1475de410b16878dc0a0515e93a2c7e556
FenUwU <76456800+fenuwu@users.noreply.github.com>
2020-12-23 10:12:01 -0500

    added basic exception handling to embed builder

--- lectionary/armenian.py
+++ lectionary/armenian.py
@@ -2,6 +2,8 @@ import helpers.bible_url
 import helpers.date_expand
 
 from discord import Embed
+from discord import Colour
+
 from bs4 import BeautifulSoup
 
 import requests
@@ -13,34 +15,13 @@ class ArmenianLectionary:
         self.bible_version = "nrsv"
 
 
-    def _build_url(self, dateobject):
-        int_month  = {
-            1  : 'january',
-            2  : 'february',
-            3  : 'march',
-            4  : 'april',
-            5  : 'may',
-            6  : 'june',
-            7  : 'july',
-            8  : 'august',
-            9  : 'september',
-            10 : 'october',
-            11 : 'november',
-            12 : 'december'
-        }
-
-        url = 'https://vemkar.us/<year>/<month>/<day>/<monthname>-<day>-<year>'
-        url = url.replace('<year>'     , str(dateobject.year))
-        url = url.replace('<month>'    , str(dateobject.month))
-        url = url.replace('<day>'      , str(dateobject.day))
-        url = url.replace('<monthname>', str(int_month[dateobject.month]))
-        return url
+    def _build_url(self):
+        today = datetime.datetime.today()
+        return today.strftime(f'https://vemkar.us/%B-{today.day}-%Y')
 
 
     def _request_data(self):
-        today = datetime.date.today()
-
-        url = self._build_url(today)
+        url = self._build_url()
 
         r = requests.get(url)
         if r.status_code != 200: return {}
@@ -60,24 +41,23 @@ class ArmenianLectionary:
         readings = readings.split('\n')
 
         return {
+            'url'      : url,
             'title'    : title,
             'readings' : readings
         }
 
 
     def build_embeds(self):
-        data = self._request_data()
-        # I need to add code here to catch if the data request failed
-
-        title    = data['title']
-        readings = data['readings']
+        try:
+            data     = self._request_data()
+            url      = data['url']
+            title    = data['title']
+            readings = data['readings']
+        except KeyError:
+            return []
 
         embed = Embed(title=title)
-
-        today = datetime.date.today()
-        url = self._build_url(today)
-        
-        embed.set_author(name='Armenian Lectionary', url='https://vemkar.us/category/lectionary')
+        embed.set_author(name='Armenian Lectionary', url=url)
 
         today = datetime.date.today()
         embed.description = helpers.date_expand.expand(today)
