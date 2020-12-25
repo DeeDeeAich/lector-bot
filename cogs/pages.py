fca7440165b22b0718648738226a4d684ae84d1c
FenUwU <76456800+fenuwu@users.noreply.github.com>
2020-12-25 10:53:33 -0500

    added copyright command

--- cogs/pages.py
+++ cogs/pages.py
@@ -1,6 +1,8 @@
 from discord import Embed
 from discord.ext import commands
 
+import datetime
+
 
 class Pages(commands.Cog):
     def __init__(self, bot):
@@ -8,23 +10,31 @@ class Pages(commands.Cog):
         self.about_page.description = 'A Discord bot for fetching and subscribing to various Christian lectionaries.'
         self.about_page.add_field(name='Developer', value='Fen#0087')
         self.about_page.add_field(name='Version', value='Alpha')
-        self.about_page.add_field(name='Library', value='[discord.py](https://github.com/Rapptz/discord.py)\nType "+mit"')
+        self.about_page.add_field(name='Library', value='[discord.py](https://github.com/Rapptz/discord.py)')
 
         sources = [
             '[Holy Trinity Russian Orthodox Church](https://www.holytrinityorthodox.com/calendar/)',
-            '[United States Conference of Catholic Bishops](https://bible.usccb.org/)',
-            '[Vanderbilt University, Divinity Library](https://lectionary.library.vanderbilt.edu/)',
+            '[USCCB](https://bible.usccb.org/)',
+            '[Vanderbilt Univerity, Divinity Library](https://lectionary.library.vanderbilt.edu/)',
             '[VEMKAR](https://vemkar.us/lectionary/)'
         ]
 
         self.about_page.add_field(name='Liturgical Sources', value='\n'.join(sources))
 
+        self.copyright_page = Embed(title='Copyright Notices')
+        self.copyright_page.add_field(name='Armenian Lectionary'      , value='Copyright © 2017-2018 Vemkar.\n')
+        self.copyright_page.add_field(name='Catholic Lectionary'      , value='Lectionary for Mass for Use in the Dioceses of the United States, second typical edition, Copyright © 2001, 1998, 1997, 1986, 1970 Confraternity of Christian Doctrine; Psalm refrain © 1968, 1981, 1997, International Committee on English in the Liturgy, Inc. All rights reserved. Neither this work nor any part of it may be reproduced, distributed, performed or displayed in any medium, including electronic or digital, without permission in writing from the copyright owner.')
+        self.copyright_page.add_field(name='Orthodox Lectionary'      , value=datetime.datetime.today().strftime('Copyright © %Y Holy Trinity Russian Orthodox Church.'))
+        self.copyright_page.add_field(name='Revised Common Lectionary', value='Revised Common Lectionary, Copyright © 1992 Consultation on Common Texts. Used by permission.')
+
         self.help_page = Embed(title='Command Help')
 
         pre = '+'
+
         self.help_page.description = '**Informational**'
-        self.help_page.description += f'\n{pre}about - Bot information'
-        self.help_page.description += f'\n{pre}help - Shows this help page'
+        self.help_page.description += f'\n{pre}about - Bot info'
+        self.help_page.description += f'\n{pre}copyright - Lectionary copyright info'
+        self.help_page.description += f'\n{pre}help - This help page'
         self.help_page.description += '\n\n**Lectionaries**'
         self.help_page.description += f'\n{pre}[armenian|a|arm|armen]'
         self.help_page.description += f'\n{pre}[catholic|c|cath]'
@@ -39,6 +49,11 @@ class Pages(commands.Cog):
     @commands.command()
     async def about(self, ctx):
         await ctx.send(embed=self.about_page)
+    
+
+    @commands.command()
+    async def copyright(self, ctx):
+        await ctx.send(embed=self.copyright_page)
 
 
     @commands.command()
