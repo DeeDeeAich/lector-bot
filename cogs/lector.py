46c3022a48f46b54562ef6d8386065fbbd6b3f42
FenUwU <76456800+fenuwu@users.noreply.github.com>
2020-12-28 15:21:26 -0500

    fixed typo in fufill_subscriptions()

--- cogs/lector.py
+++ cogs/lector.py
@@ -204,7 +204,7 @@ class Lectionary(commands.Cog):
     @tasks.loop(minutes=10)
     async def fufill_subscriptions(self):
         # Push today's subscriptions on or after 2AM if they haven't been already
-        if (self.last_fufilly != datetime.date.today) and (datetime.datetime.now().hour >= 2):
+        if (self.last_fufill != datetime.date.today) and (datetime.datetime.now().hour >= 2):
             # Make sure the cached lectionary embeds are updated
             self.regenerate_all_data()
             self.build_all_embeds()
