b9aec57185fb2c1b61c921736e60a21789ab43b3
FenUwU <76456800+fenuwu@users.noreply.github.com>
2020-12-28 15:15:10 -0500

    removed Bible versioning and view mode

--- cogs/lector.py
+++ cogs/lector.py
@@ -19,15 +19,18 @@ class Lectionary(commands.Cog):
 
         # This list is for indexing-display purposes
         # In the database, 0 through 3 coorspond to these
-        self.l = ['Armenian', 'Catholic', 'Orthodox', 'Revised Common']
+        self.lectionary_names = [
+            'armenian',
+            'catholic',
+            'orthodox',
+            'revised common']
 
         # Lectionary Objects
-        self.armenian_lectionary = armenian.ArmenianLectionary()
-        self.catholic_lectionary = catholic.CatholicLectionary()
-        self.orthodox_lectionary = orthodox.OrthodoxLectionary()
-        self.rcl_lectionary      = rcl.RevisedCommonLectionary()
+        self.armenian = armenian.ArmenianLectionary()
+        self.catholic = catholic.CatholicLectionary()
+        self.orthodox = orthodox.OrthodoxLectionary()
+        self.rcl      = rcl.RevisedCommonLectionary()
 
-        # Build initial embeds
         self.build_all_embeds()
 
         # Set up the subscription database if it isn't already
@@ -41,60 +44,45 @@ class Lectionary(commands.Cog):
         self.fufill_subscriptions.start()
 
     
+    def regenerate_all_data(self):
+        self.armenian.regenerate_data()
+        self.catholic.regenerate_data()
+        self.orthodox.regenerate_data()
+        self.rcl.regenerate_data()
+
+
     def build_all_embeds(self):
-        self.armenian_embeds  = self.armenian_lectionary.build_embeds()
-        self.catholic_embeds  = self.catholic_lectionary.build_embeds()
-        self.orthodox_embeds  = self.orthodox_lectionary.build_embeds()
-        self.rcl_embeds       = self.rcl_lectionary.build_embeds()
+        self.feeds = [
+            self.armenian.build_embeds(),
+            self.catholic.build_embeds(),
+            self.orthodox.build_embeds(),
+            self.rcl.build_embeds()
+        ]
 
 
     '''BASIC LECTIONARY REQUEST COMMANDS'''
 
     @commands.command(aliases=['a', 'arm', 'armen'])
     async def armenian(self, ctx):
-        if self.armenian_embeds == []:
-            self.armenian_embeds  = self.armenian_lectionary.build_embeds()
-            if self.armenian_embeds == []:
-                await ctx.send(embed=self.error_embed)
-                return
-
-        for embed in self.armenian_embeds:
+        for embed in self.feeds[0]:
             await ctx.send(embed=embed)
 
 
     @commands.command(aliases=['c', 'cath'])
     async def catholic(self, ctx):
-        if self.catholic_embeds == []:
-            self.catholic_embeds  = self.catholic_lectionary.build_embeds()
-            if self.catholic_embeds == []:
-                await ctx.send(embed=self.error_embed)
-                return
-
-        for embed in self.catholic_embeds:
+        for embed in self.feeds[1]:
             await ctx.send(embed=embed)
 
 
     @commands.command(aliases=['o', 'orth', 'ortho'])
     async def orthodox(self, ctx):
-        if self.orthodox_embeds == []:
-            self.orthodox_embeds  = self.orthodox_lectionary.build_embeds()
-            if self.orthodox_embeds == []:
-                await ctx.send(embed=self.error_embed)
-                return
-
-        for embed in self.orthodox_embeds:
+        for embed in self.feeds[2]:
             await ctx.send(embed=embed)
-    
+
 
     @commands.command(aliases=['r', 'revised', 'prot', 'protestant'])
-    async def rcl(self, ctx, arg:str=None):
-        if self.rcl_embeds == []:
-            self.rcl_embeds = self.rcl_lectionary.build_embeds()
-            if self.rcl_embeds == []:
-                await ctx.send(embed=self.error_embed)
-                return
-
-        for embed in self.rcl_embeds:
+    async def rcl(self, ctx):
+        for embed in self.feeds[3]:
             await ctx.send(embed=embed)
     
 
@@ -102,11 +90,11 @@ class Lectionary(commands.Cog):
 
     @commands.command(aliases=['sub'])
     @commands.has_permissions(manage_messages=True)
-    async def subscribe(self, ctx, arg: str, channel: typing.Optional[discord.TextChannel] = None):
-        if   arg in ['armenian','a','arm','armen']            : sub_type = 0
-        elif arg in ['catholic','c','cath']                   : sub_type = 1
-        elif arg in ['orthodox','o','orth','ortho']           : sub_type = 2
-        elif arg in ['rcl','r','revised','prot','protestant'] : sub_type = 3
+    async def subscribe(self, ctx, lectionary, channel:typing.Optional[discord.TextChannel] = None):
+        if   lectionary in ['armenian','a','arm','armen']            : sub_type = 0
+        elif lectionary in ['catholic','c','cath']                   : sub_type = 1
+        elif lectionary in ['orthodox','o','orth','ortho']           : sub_type = 2
+        elif lectionary in ['rcl','r','revised','prot','protestant'] : sub_type = 3
         else:
             await ctx.send('You didn\'t specify a valid lectionary option.')
             return
@@ -114,30 +102,33 @@ class Lectionary(commands.Cog):
         conn = sqlite3.connect('data.db')
         c    = conn.cursor()
 
-        if channel:
-            channel_id = channel.id
-        else:
-            channel_id = ctx.channel.id
+        if channel: channel_id = channel.id
+        else:       channel_id = ctx.channel.id
 
         server_id = ctx.guild.id
 
         # Check to see if a subscription for the channel already exists
         c.execute('SELECT * FROM subscriptions WHERE channel_id = ?', (channel_id,))
         row = c.fetchone()
-        sub_name = self.l[sub_type]
+        sub_name = self.lectionary_names[sub_type].title()
 
+        # If a subscription already exists for the current channel
         if row:
-            # If a subscription already exists for the current channel
-            if row[2] == sub_type:
-                # If the channel is already subscribed to what was requested
+            # If channel subscription request would not actually change any settings
+            if (row[2] == sub_type) and (row[3] == view_mode):
                 await ctx.send(f'<#{channel_id}> is already subscribed to the {sub_name} Lectionary.')
             else:
-                # If the subscription for a channel is changing from one lectionary to another
-                c.execute('UPDATE subscriptions SET sub_type = ? WHERE channel_id = ?', (sub_type, channel_id))
+                # If the subscription for a channel is changing somehow
+                c.execute('''
+                    UPDATE subscriptions
+                    SET sub_type = ?
+                    WHERE channel_id = ?''',
+                    (sub_type, channel_id))
                 conn.commit()
                 await ctx.send(f'The subscription for <#{channel_id}> has been updated to the {sub_name} Lectionary.')
+
+        # If a subscription does not exist for the channel
         else:
-            # If a subscription does not exist for the channel
             c.execute('INSERT INTO subscriptions VALUES (?, ?, ?)', (server_id, channel_id, sub_type))
             conn.commit()
             await ctx.send(f'<#{channel_id}> has been subscribed to the {sub_name} Lectionary.')
@@ -169,7 +160,8 @@ class Lectionary(commands.Cog):
                 await ctx.send(f'<#{channel_id}> has been unsubscribed.')
             else:
                 await ctx.send(f'<#{channel_id}> is not subscribed to any lectionaries.')
-        elif isinstance(channel, str) and channel == 'all':
+        
+        elif isinstance(channel, str) and (channel == 'all'):
             c.execute('DELETE FROM subscriptions WHERE server_id = ?', (ctx.guild.id,))
             conn.commit()
             await ctx.send(f'All subscriptions for {ctx.guild.name} have been removed.')
@@ -188,7 +180,9 @@ class Lectionary(commands.Cog):
 
         embed = discord.Embed(title=f'Subscriptions for {ctx.guild.name}')
         if subscriptions:
-            embed.description = '\n'.join([f'<#{sub[1]}> - {self.l[sub[2]]} Lectionary' for sub in subscriptions])
+            embed.description = '\n'.join([
+                f'<#{sub[1]}> - {self.lectionary_names[sub[2]].title()} Lectionary'
+                for sub in subscriptions])
         else:
             embed.description = 'There are none'
         
@@ -210,8 +204,9 @@ class Lectionary(commands.Cog):
     @tasks.loop(minutes=10)
     async def fufill_subscriptions(self):
         # Push today's subscriptions on or after 2AM if they haven't been already
-        if self.last_fufill != datetime.date.today() and datetime.datetime.now().hour >= 2:
+        if (self.last_fufilly != datetime.date.today) and (datetime.datetime.now().hour >= 2):
             # Make sure the cached lectionary embeds are updated
+            self.regenerate_all_data()
             self.build_all_embeds()
 
             conn = sqlite3.connect('data.db')
@@ -219,18 +214,13 @@ class Lectionary(commands.Cog):
             c.execute('SELECT * FROM subscriptions')
             subscriptions = c.fetchall()
 
-            feeds = [self.armenian_embeds, self.catholic_embeds, self.orthodox_embeds, self.rcl_embeds]
             for subscription in subscriptions:
-                channel_id, sub_type = subscription[1], subscription[2]
-                channel = self.bot.get_channel(channel_id)
+                channel  = self.bot.get_channel(subscription[1])
+                sub_type = subscription[2]
 
-                # If channel exists and is accessible to the bot
                 if channel:
-                    if feeds[sub_type] != []:
-                        for embed in feeds[sub_type]:
-                            await channel.send(embed=embed)
-                    else:
-                        await channel.send(embed=self.error_embed)
+                    for embed in self.feeds[sub_type]:
+                        await channel.send(embed=embed)
                 else:
                     c.execute('DELETE FROM subscriptions WHERE channel_id = ?', (channel_id,))
                         
