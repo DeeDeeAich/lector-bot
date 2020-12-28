3e59a57d580ed760cf14af14499c201b4e28c209
FenUwU <76456800+fenuwu@users.noreply.github.com>
2020-12-28 11:43:12 -0500

    Updated help page

--- cogs/pages.py
+++ cogs/pages.py
@@ -10,16 +10,8 @@ class Pages(commands.Cog):
         self.about_page.description = 'A Discord bot for fetching and subscribing to various Christian lectionaries.'
         self.about_page.add_field(name='Developer', value='Fen#0087')
         self.about_page.add_field(name='Version', value='Alpha')
+        self.about_page.add_field(name='Repository', value='[lector-bot](https://github.com/fenuwu/lector-bot)')
         self.about_page.add_field(name='Library', value='[discord.py](https://github.com/Rapptz/discord.py)')
-
-        sources = [
-            '[Holy Trinity Russian Orthodox Church](https://www.holytrinityorthodox.com/calendar/)',
-            '[USCCB](https://bible.usccb.org/)',
-            '[Vanderbilt Univerity, Divinity Library](https://lectionary.library.vanderbilt.edu/)',
-            '[VEMKAR](https://vemkar.us/lectionary/)'
-        ]
-
-        self.about_page.add_field(name='Liturgical Sources', value='\n'.join(sources))
 
         self.copyright_page = Embed(title='Copyright Notices')
         self.copyright_page.add_field(name='Armenian Lectionary'      , value='The Liturgical Calendar of the Armenian Church (Lectionary), Copyright © 2017-2018 Vemkar.\n', inline=False)
@@ -28,20 +20,18 @@ class Pages(commands.Cog):
         self.copyright_page.add_field(name='Revised Common Lectionary', value='Revised Common Lectionary, Copyright © 1992 Consultation on Common Texts. Used by permission.', inline=False)
 
         self.help_page = Embed(title='Command Help')
-
         pre = bot.command_prefix
-
         self.help_page.description = '**Informational**'
         self.help_page.description += f'\n{pre}about - Bot info'
         self.help_page.description += f'\n{pre}copyright - Copyright info'
         self.help_page.description += f'\n{pre}help - This help page'
         self.help_page.description += '\n\n**Lectionaries**'
-        self.help_page.description += f'\n{pre}[armenian|a|arm|armen]'
-        self.help_page.description += f'\n{pre}[catholic|c|cath]'
-        self.help_page.description += f'\n{pre}[orthodox|o|orth|ortho]'
-        self.help_page.description += f'\n{pre}[rcl|r|revised|prot|protestant]'
+        self.help_page.description += f'\n{pre}[armenian|a|arm|armen] [*Bible version*] (print|normal)'
+        self.help_page.description += f'\n{pre}[catholic|c|cath] [*Bible version*] (print|normal)'
+        self.help_page.description += f'\n{pre}[orthodox|o|orth|ortho] [*Bible version*] (print|normal)'
+        self.help_page.description += f'\n{pre}[rcl|r|revised|prot|protestant] [*Bible version*] (print|normal)'
         self.help_page.description += '\n\n**Subscriptions (Requires Manage Messages)**'
-        self.help_page.description += f'\n{pre}[subscribe|sub] (a|c|o|r) (#channel)'
+        self.help_page.description += f'\n{pre}[subscribe|sub] [a|c|o|r] [*Bible version*] [print|normal] (#channel)'
         self.help_page.description += f'\n{pre}[unsubscribe|unsub] (#channel|all)'
         self.help_page.description += f'\n{pre}[subscriptions|subs]'
 
