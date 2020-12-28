23e020d81e77bbf0e0056749d329bc77b230af25
FenUwU <76456800+fenuwu@users.noreply.github.com>
2020-12-28 11:57:45 -0500

    revamped OrthodoxLectionary class

--- lectionary/orthodox.py
+++ lectionary/orthodox.py
@@ -1,5 +1,5 @@
-import helpers.bible_url
-import helpers.date_expand
+from helpers import bible_url
+from helpers import date_expand
 
 from discord import Embed
 from bs4 import BeautifulSoup
@@ -11,57 +11,59 @@ import datetime
 
 class OrthodoxLectionary:
     def __init__(self):
-        self.bible_version = 'nrsv'
+        self.regenerate_data()
   
 
-    def _build_url(self):
+    def clear_data(self):
+        self.url       = ''
+        self.title     = ''
+        self.subtitles = []
+        self.saints    = []
+        self.readings  = []
+        self.troparion = {}
+
+
+    def regenerate_data(self):
         today = datetime.datetime.today()
-        return f'https://www.holytrinityorthodox.com/calendar/calendar.php?month={today.month}&today={today.day}&year={today.year}&dt=1&header=1&lives=1&trp=1&scripture=1'
+        self.url = f'https://www.holytrinityorthodox.com/calendar/calendar.php?month={today.month}&today={today.day}&year={today.year}&dt=1&header=1&lives=1&trp=1&scripture=1'
 
-
-    def _request_data(self):
-        '''
-        Helper method to scrape the webpage containing the calendar data.
-        '''
-
-        url = self._build_url()
-
-        r = requests.get(url)
-        if r.status_code != 200: return {}
-
+        r = requests.get(self.url)
+        if r.status_code != 200:
+            self.clear_data()
+            return
+        
         soup = BeautifulSoup(r.text, 'html.parser')
 
-        # Title and subtitle
-        title     = soup.select_one('span[class="dataheader"]').text
-        a         = soup.select_one('span[class="headerheader"]').text
-        b         = soup.select_one('span[class="headerfast"]').text
-        subtitles = [a.replace(b,''), b.strip()]
-        
-        # Saint Section
-        saints = [
+        # Title & subtitles
+        self.title     = soup.select_one('span[class="dataheader"]').text
+        a              = soup.select_one('span[class="headerheader"]').text
+        b              = soup.select_one('span[class="headerfast"]').text
+        self.subtitles = [a.replace(b,''), b.strip()]
+
+        # Saints
+        self.saints = [
             item
             for item in soup.select_one('span[class="normaltext"]').text.split('\n')
-            if item]
+            if item
+        ]
 
-        # Readings Section
+        # Readings
         readings = soup.select_one('span[class="normaltext"]:nth-child(5)')
         readings = [str(item) for item in readings.contents]
         readings = ''.join(readings)
         readings = readings.replace('\n','')
-        readings = readings.split('<br/>')
-        readings = [reading for reading in readings if reading != '']
+        readings = readings.replace('<br/>', '\n')
+        if readings[-1] == '\n': readings = readings[:-1]
 
-        for index, reading in enumerate(readings):
-            match = re.search(r'<a.*>(.*)<\/a>', reading)
-            if match:
-                # 'tag' looks like '<a>Matthew 5:11</a>'
-                # 'ref' looks like 'Matthew 5:11'
-                tag, ref = match.group(0), match.group(1)
-                link = helpers.bible_url.convert(ref, self.bible_version)
-                readings[index] = reading.replace(tag, link)
+        matches = re.findall(r'(<a.*>([^<>]*)<\/a>)', readings)
+        for match in matches:
+            readings = readings.replace(match[0], f'<a>{match[1]}</a>')
+        
+        self.readings = readings
 
-        # Troparion Section
-            # keys represent the saint name and tone number
+        # Troparion
+            # keys represent saint/tone
+            # values represent troparion contents
         keys = [item.text for item in soup.select('p > b:first-child')]
         values = [item.text for item in soup.select('span[class="normaltext"] > p')]
         values = [value.replace(key, '') for key, value in zip(keys, values)]
@@ -69,58 +71,39 @@ class OrthodoxLectionary:
         keys   = [key.replace('\n','').replace('\r','').replace(' —','') for key in keys]
         values = [value.replace('\n','').replace('\r','') for value in values]
 
-        troparions = {}
+        self.troparion = {}
         for key, value in zip(keys, values):
-            troparions[key] = value
-
-        return {
-            'url'        : url,
-            'title'      : title,
-            'subtitles'  : subtitles,
-            'saints'     : saints,
-            'readings'   : readings,
-            'troparions' : troparions
-        }
+            self.troparion[key] = value
     
 
-    def build_embeds(self):
+    def build_embeds(self, bible_version, view_mode):
         '''
         "Public" function to construct a list of Discord Embeds representing
         the calendar data.
-        '''        
-        try:
-            data       = self._request_data()
-            url        = data['url']
-            title      = data['title']
-            subtitles  = data['subtitles']
-            saints     = data['saints']
-            readings   = data['readings']
-            troparions = data['troparions']
-        except KeyError:
-            return []
+        '''
         
         embeds = []
 
-        # Title Embed
-        title_embed = Embed(title=title)
-        title_embed.set_author(name='Orthodox Calendar', url=url)
-        title_embed.description = '\n'.join(subtitles)
-        embeds.append(title_embed)
+        # Title & Subtitles Embed
+        embed = Embed(title=self.title)
+        embed.set_author(name='Orthodox Calendar', url=self.url)
+        embed.description = '\n'.join(self.subtitles)
+        embeds.append(embed)
         
         # Saints Embed
-        saints_embed = Embed(title='The Saints')
-        saints_embed.description = '\n'.join(saints)
-        embeds.append(saints_embed)
+        embed = Embed(title='The Saints')
+        embed.description = '\n'.join(self.saints)
+        embeds.append(embed)
 
         # Readings Embed
-        scripture_embed = Embed(title='The Scripture Readings')
-        scripture_embed.description = '\n'.join(readings)
-        embeds.append(scripture_embed)
+        embed = Embed(title='The Scripture Readings')
+        embed.description = bible_url.html_convert(self.readings, bible_version, view_mode)
+        embeds.append(embed)
 
         # Troparion Embed
-        troparion_embed = Embed(title='Troparion')
-        for saint in troparions.keys():
-            troparion_embed.add_field(name=saint, value=troparions[saint], inline=False)
-        embeds.append(troparion_embed)
+        embed = Embed(title='Troparion')
+        for saint in self.troparion.keys():
+            embed.add_field(name=saint, value=self.troparion[saint], inline=False)
+        embeds.append(embed)
 
         return embeds
